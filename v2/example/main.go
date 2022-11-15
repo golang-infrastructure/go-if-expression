821882f904cd048497e8e0b8e97784dd3f5cd60f
CC11001100 <CC11001100@qq.com>
2022-11-15 14:36:57 +0800

    refactor

--- v2/example/main.go
+++ v2/example/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/golang-infrastructure/go-if-expression/v2/if_expression"
+	"github.com/golang-infrastructure/go-if-expression/v2/pkg/if_expression"
 )
 
 func main() {
